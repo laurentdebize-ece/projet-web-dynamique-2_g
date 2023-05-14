136e275a89b2a90e6e93ecfed3106a86f7006d0d
YouriLalain <youri.lalain@edu.ece.fr>
2023-05-15 00:52:01 +0200

    MAJ matiere

--- scriptMatiere.js
+++ scriptMatiere.js
@@ -1,4 +1,27 @@
 $(document).ready(function(){
+    
+    $('#boutton1').click(function(){
+        $('#mat1').toggle;
+    });
+    $('#boutton2').click(function(){
+        $('#mat2').toggle();
+    });
+    $('#boutton3').click(function(){
+        $('#mat3').toggle();
+    });
+    $('#boutton4').click(function(){
+        $('#mat4').toggle();
+    });
+    $('#boutton5').click(function(){
+        $('#mat5').toggle();
+    });
+    $('#boutton6').click(function(){
+        $('#mat6').toggle();
+    });
+    $('#boutton7').click(function(){
+        $('#mat7').toggle();
+    });
+    
     $('#boutton1').click(function(){
         $('#comp1').toggle()
     });
