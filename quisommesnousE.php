fc9bab6d8d73eae55e88391aac2188b8b9543dd4
soleneaz <solene.azemar@edu.ece.fr>
2023-05-24 12:02:02 +0200

    css

--- quisommesnousE.php
+++ quisommesnousE.php
@@ -2,61 +2,103 @@
 session_start();
 ?>
 
+<!DOCTYPE html>
+<html lang="fr">
 <head>
-	<meta charset="UTF-8">
-	<title>Mentions légales</title>
-	<link rel="stylesheet" type="text/css" href="quisommesnous.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>À propos de nous</title>
+    <link rel="stylesheet" type="text/css" href="quisommesnousE.css">
 </head>
+
 <body>
+
     <?php include("headerE.php"); ?>
     <?php include("footerE.php"); ?>
-    <div class="content">
-        <section class="section">
-            <h2>Notre histoire</h2>
-            <p>istique, velit id consequat pharetra, nulla quam commodo nulla, vel consectetur tortor purus eget turpis.",
-        "Integer ac sapien eget leo molestie faucibus.",
-        "Sed dapibus tincidunt tellus, in molestie velit molestie sit amet.",
-        "Sed vel nunc bibendum, elementum neque id, dictum eros.",
-        "Fusce luctus erat vel dapibus vestibulum.",
-        "Pellentesque eu felis eget nulla auctor semper.",
-        "Nulla facilisi.",
-        "Morbi volutpat sapien enim, non blan</p>
-        </section>
 
-        <section class="section">
-            <h2>Qui sommes nous</h2>
-            <p>istique, velit id consequat pharetra, nulla quam commodo nulla, vel consectetur tortor purus eget turpis.",
-        "Integer ac sapien eget leo molestie faucibus.",
-        "Sed dapibus tincidunt tellus, in molestie velit molestie sit amet.",
-        "Sed vel nunc bibendum, elementum neque id, dictum eros.",
-        "Fusce luctus erat vel dapibus vestibulum.",
-        "Pellentesque eu felis eget nulla auctor semper.",
-        "Nulla facilisi.",
-        "Morbi volutpat sapien enim, non blan</p>
-        </section>
+    <div class="container">
 
-        <section class="section">
-            <h2>Notre équipe</h2>
-            <p>istique, velit id consequat pharetra, nulla quam commodo nulla, vel consectetur tortor purus eget turpis.",
-        "Integer ac sapien eget leo molestie faucibus.",
-        "Sed dapibus tincidunt tellus, in molestie velit molestie sit amet.",
-        "Sed vel nunc bibendum, elementum neque id, dictum eros.",
-        "Fusce luctus erat vel dapibus vestibulum.",
-        "Pellentesque eu felis eget nulla auctor semper.",
-        "Nulla facilisi.",
-        "Morbi volutpat sapien enim, non blan</p>
+        <section>
+            <h2 class="titre1">Notre histoire</h2>
+            <div class="section-content">
+                <div class="column">
+                    <div class="image-column">
+                        <img src="images/imageE1.jpg" alt="image">
+                    </div>
+                    <div class="text-column">
+                        <p>
+                            Notre histoire commence par une simple question : comment pouvons-nous aider les élèves à évaluer leurs compétences de manière plus efficace et pertinente ? C'est ainsi qu'est née l'idée de créer cette plateforme en ligne, offrant aux étudiants un moyen pratique de suivre leur développement personnel. Depuis notre lancement, nous avons travaillé sans relâche pour améliorer continuellement notre système, en nous adaptant aux besoins changeants des élèves et des enseignants.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </section>
-
-        <section class="section">
-            <h2>Notre vision</h2>
-            <p>istique, velit id consequat pharetra, nulla quam commodo nulla, vel consectetur tortor purus eget turpis.",
-        "Integer ac sapien eget leo molestie faucibus.",
-        "Sed dapibus tincidunt tellus, in molestie velit molestie sit amet.",
-        "Sed vel nunc bibendum, elementum neque id, dictum eros.",
-        "Fusce luctus erat vel dapibus vestibulum.",
-        "Pellentesque eu felis eget nulla auctor semper.",
-        "Nulla facilisi.",
-        "Morbi volutpat sapien enim, non blan</p>
+<br>
+        <section>
+            <h2 class="titre">Qui sommes nous?</h2>
+            <div class="section-content">
+                <div class="column">
+                    <div class="text-column">
+                        <p>
+                        Nous sommes une équipe dévouée d'éducateurs et de développeurs passionnés par l'éducation et la croissance personnelle des élèves. Forts de notre expérience variée dans le domaine de l'enseignement, nous avons combiné nos compétences pour créer cette plateforme innovante. Notre objectif est de fournir un outil convivial, transparent et fiable pour l'évaluation des compétences, permettant ainsi aux élèves de prendre en main leur parcours éducatif.
+                        </p>
+                    </div>
+                    <div class="img1">
+                        <div class="image-column">
+                            <img src="images/question.png" alt="question">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+<br>
+        <section>
+            <h2 class="titre">Notre équipe</h2>
+            <div class="section-content">
+                <p>
+                    Notre équipe est composée d'experts en éducation, de développeurs talentueux et de spécialistes en conception d'expérience utilisateur. Chaque membre de notre équipe apporte des compétences uniques et une passion commune pour l'amélioration de l'éducation. Nous travaillons en étroite collaboration avec les enseignants et les établissements scolaires pour comprendre leurs besoins spécifiques et offrir des solutions adaptées.
+                </p>
+                <br><br>
+                <div class="avatars">
+                    <div class="avatar">
+                        <img src="images/avatarClement.png" alt="Avatar Clement">
+                        <p>Clément HERBIN</p>
+                    </div>
+                    <div class="avatar">
+                        <img src="images/avatarYouri.png" alt="Avatar Youri">
+                        <p>Youri LALAIN</p>
+                    </div>
+                    <div class="avatar">
+                        <img src="images/avatarMael.png" alt="Avatar Mael">
+                        <p>Maël DEPREVILLE</p>
+                    </div>
+                    <div class="avatar">
+                        <img src="images/avatarSolene.png" alt="Avatar Solene">
+                        <p>Solene AZEMAR</p>
+                    </div>
+                    <div class="avatar">
+                        <img src="images/avatarNina.png" alt="Avatar Nina">
+                        <p>Nina NICCOLINI</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+<br><br>
+        <section>
+            <h2 class="titre1">Notre vision</h2>
+            <div class="section-content">
+                <div class="column">
+                    <div class="image-column">
+                        <img src="images/vision.jpg" alt="image croissance">
+                    </div>
+                    <div class="text-column">
+                        <p>
+                        Notre vision est de créer un environnement d'apprentissage global, où chaque élève est encouragé à développer ses compétences de manière personnalisée. Nous croyons fermement que l'évaluation des compétences ne devrait pas se limiter à des notes et des résultats scolaires traditionnels, mais plutôt à une approche globale qui tient compte des talents, des aptitudes et des intérêts uniques de chaque élève. Notre plateforme permet aux enseignants d'ajouter, de supprimer et de valider les compétences des élèves, favorisant ainsi une évaluation plus précise et une collaboration transparente.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </section>
     </div>
 </body>
+</html>
